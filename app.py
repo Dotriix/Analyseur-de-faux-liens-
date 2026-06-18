be626c232701edd7943aba155a1f577531e12a7c
Dotriix <74355747+Dotriix@users.noreply.github.com>
2026-06-18 15:42:39 +0400

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def check_checklink(url_to_scan):
     # Si le site est très récent ou contient des mots clés bancaires frauduleux
     mots_cles_arnaque = ["facturation", "prime", "ameli", "caf", "colis", "infractions"]
     if any(mot in url_to_scan.lower() for mot in mots_cles_arnaque):
-        return {"status": "danger", "raison": "Détection d'un mot-clé de phishing connu par O.R.I.O.N."}
+        return {"status": "danger", "raison": "Détection d'un mot-clé d'hameçonnage connu par O.R.I.O.N."}
     return {"status": "safe", "raison": "Aucune anomalie visuelle immédiate."}
 
 @app.route('/scan', methods=['POST'])
@@ -58,7 +58,7 @@ def scan_url():
         details = f"O.R.I.O.N a détecté : {vt_result['detec']} alerte(s). {cl_result['raison']}"
     else:
         verdict = "✅ SÉCURISÉ ✅"
-        details = f"O.R.I.O.N n'a détecté aucune anomalie."
+        details = "O.R.I.O.N n'a détecté aucune anomalie."
         
     return jsonify({
         "verdict": verdict,
