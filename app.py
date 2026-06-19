e8958f3b44793625fe4991cacf62013909d24dbe
Dotriix <74355747+Dotriix@users.noreply.github.com>
2026-06-19 11:51:30 +0400

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def scan_url():
     
     # Correction de l'indentation ici (tout doit être bien aligné)
     if vt["status"] == "danger" or cl["status"] == "danger":
-        verdict ="              ⚠️ LIEN SUSPECT ⚠️"
+        verdict ="             ⚠️ LIEN SUSPECT ⚠️"
         
         # On personnalise le message
         if vt["status"] == "danger" and cl["status"] == "danger":
